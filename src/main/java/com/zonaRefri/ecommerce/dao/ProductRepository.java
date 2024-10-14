60ab1c43eb6869d94f7425d2f6d393c3ccfc6d00
aaguada <alex.aguada@snoopconsulting.com>
2024-10-14 14:26:09 -0300

    se comienza con el test de metodos http

---
+++ src/main/java/com/zonaRefri/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.zonaRefri.ecommerce.dao;
+import com.zonaRefri.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
