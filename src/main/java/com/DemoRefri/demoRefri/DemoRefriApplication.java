7c29abd80b8300dc2f14f5c1532d4996c49496ae
alex <alex.david.aguada.jura@gmail.com>
2025-06-02 14:53:09 -0300

    inicio de ecommerce

---
+++ src/main/java/com/DemoRefri/demoRefri/DemoRefriApplication.java
@@ -0,0 +1,13 @@
+package com.DemoRefri.demoRefri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRefriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRefriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DemoRefri/demoRefri/DemoRefriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DemoRefri.demoRefri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRefriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
